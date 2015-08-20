2fb06c16ec76bd09c8cd00f0c1dc4f7b202a50a0
Calvyn82 <claytonflesher@gmail.com>
2015-08-19 21:22:35 -0500

    created controllers, views and routes for users and sessions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,15 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Email or password didn't match"
+      render :new
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Rails.application.routes.draw do
+  get "signup" => "users#new", as: :signup
+  post "signup" => "user#create"
+
+  get "signin" => "sessions#new", as: :signin
+  post "signin" => "sessions#create"
+
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'tracker#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
