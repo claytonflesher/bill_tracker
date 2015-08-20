434f9536520a1a0bc7948d9f36026938009fa5da
Calvyn82 <claytonflesher@gmail.com>
2015-08-19 20:37:12 -0500

    added additional test and fixed bug in both

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,6 +4,12 @@ RSpec.describe User, type: :model do
   it "makes sure the email is present" do 
     user = User.new(email: nil)
     expect(user).not_to        be_valid
-    expect(user.errors).not_to be_empty
+    expect(user.errors[:email]).not_to be_empty
+  end
+
+  it "ensure the email is well-formed" do
+    user = User.new(email: "bad fake email")
+    expect(user).not_to        be_valid
+    expect(user.errors[:email]).not_to be_empty
   end
 end
