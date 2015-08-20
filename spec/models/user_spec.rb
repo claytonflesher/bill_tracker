aba4d404948e12b016f1dddd88aa7ca1602e0977
Calvyn82 <claytonflesher@gmail.com>
2015-08-19 20:34:23 -0500

    set up factory girl generations and got first test passing

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "makes sure the email is present" do 
+    user = User.new(email: nil)
+    expect(user).not_to        be_valid
+    expect(user.errors).not_to be_empty
+  end
 end
