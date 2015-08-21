0a98ec6f775fff62351a60271942de629e3540e5
Calvyn82 <claytonflesher@gmail.com>
2015-08-21 16:47:10 -0500

    reroutes app to signin if user isn't currently signed in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,22 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user, :logged_in?
+  before_action :require_login
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user])
+  end
+
+  def logged_in?
+    current_user != nil
+  end
+
+  private
+
+  def require_login
+    unless logged_in?
+      redirect_to :signin
+    end
+  end
 end
