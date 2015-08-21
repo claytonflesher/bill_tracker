3a48c49597445cc5e19edcac42342d30bbca530c
Calvyn82 <claytonflesher@gmail.com>
2015-08-21 17:33:52 -0500

    added bill model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150820234415) do
+ActiveRecord::Schema.define(version: 20150821221306) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bills", force: :cascade do |t|
+    t.string   "name"
+    t.text     "description"
+    t.integer  "user_id"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
+
+  add_index "bills", ["user_id"], name: "index_bills_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",           null: false
@@ -28,4 +38,5 @@ ActiveRecord::Schema.define(version: 20150820234415) do
 
   add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
+  add_foreign_key "bills", "users"
 end
