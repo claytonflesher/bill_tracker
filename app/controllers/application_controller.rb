9c8e9821ec8587dbdf4c9863711854978a8c0092
Calvyn82 <claytonflesher@gmail.com>
2015-08-21 16:51:52 -0500

    fixed bug in before_action method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,7 @@ class ApplicationController < ActionController::Base
   before_action :require_login
 
   def current_user
-    @current_user ||= User.find_by(id: session[:user])
+    @current_user ||= User.find_by(id: session[:user_id])
   end
 
   def logged_in?
