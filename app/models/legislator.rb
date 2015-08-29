ca51d6d8871d4afb2131237a107e296faeec698f
Calvyn82 <claytonflesher@gmail.com>
2015-08-28 20:22:18 -0500

    fixed bugs in Legislator

--- app/models/legislator.rb
+++ app/models/legislator.rb
@@ -1,28 +1,66 @@
 class Legislator
-  def initalize(json:, chamber:)
-    @rep_data       = filter(json)
-    @chamber        = chamber.to_s
-    @first_name     = rep_data["first_name"]
-    @middle_name    = rep_data["middle_name"]
-    @last_name      = rep_data["last_name"]
-    @full_name      = rep_data["full_name"]
-    @district       = rep_data["district"]
-    @state          = rep_data["state"]
-    @email          = rep_data["email"]
-    @party          = rep_data["party"]
-    @leg_id         = rep_data["leg_id"]
-    @url            = rep_data["url"]
-    @fax            = rep_data["offices"]["fax"]
-    @phone          = rep_data["offices"]["phone"]
-    @office_address = rep_data["offices"]["address"]
+  def initialize(json:, chamber:)
+    @chamber = chamber.to_s
+    @rep_data        = filter(json)
   end
 
-  attr_reader :chamber, :first_name, :middle_name, :last_name,
-              :full_name, :district, :state, :email, :party,
-              :leg_id, :url, :fax, :phone, :office_address
+  attr_reader :rep_data
+  private     :rep_data
+
+  def first_name
+    rep_data["first_name"]
+  end
+
+  def middle_name
+    rep_data["middle_name"]
+  end
+
+  def last_name
+    rep_data["last_name"]
+  end
+
+  def full_name
+    rep_data["full_name"]
+  end
+
+  def district
+    rep_data["district"]
+  end
+
+  def state
+    rep_data["state"]
+  end
 
   def committees
     rep_data["roles"].map { |role| role["committee"] }.compact
+  end
+
+  def email
+    rep_data["email"]
+  end
+
+  def party
+    rep_data["party"]
+  end
+
+  def leg_id
+    rep_data["leg_id"]
+  end
+
+  def url
+    rep_data["url"]
+  end
+
+  def fax
+    rep_data["offices"]["fax"]
+  end
+
+  def phone
+    rep_data["offices"]["phone"]
+  end
+
+  def address
+    rep_data["offices"]["address"]
   end
 
   private
