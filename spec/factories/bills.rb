79cbf8ef91457a2d4904063da33eb3874bc07b8e
Calvyn82 <claytonflesher@gmail.com>
2015-08-29 21:13:32 -0500

    found and fixed bug in flash

--- spec/factories/bills.rb
+++ spec/factories/bills.rb
@@ -1,8 +1,16 @@
 FactoryGirl.define do
   factory :bill do
-    name "MyString"
-description "MyText"
-user nil
+    name "HB1001"
+    description ["History of HB 1001",
+                 "Action",
+                 "Journal Page",
+                 "Date",
+                 "Chamber",
+                 "First Reading",
+                 "47",
+                 "02/02/2015",
+                 "H"]
+    user
   end
 
 end
--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -1,4 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe Bill, type: :model do
+  it "makes sure the name is present" do
+    bill = Bill.new(name: nil)
+    expect(bill).not_to               be_valid
+    expect(bill.errors[:name]).not_to be_empty
+  end
+
+  it "ensures the name is well-formed" do
+    bill = Bill.new(name: "invalid name")
+    expect(bill).not_to               be_valid
+    expect(bill.errors[:name]).not_to be_empty
+  end
+
+  it "ensure the bill is unique to that user" do
+    bill1      = FactoryGirl.create(:bill)
+    bill1.user = FactoryGirl.create(:user)
+    bill2      = Bill.new(name: bill1.name)
+    bill1.user = bill1.user
+    expect(bill1).to                   be_valid
+    expect(bill2).not_to               be_valid
+  end
 end
