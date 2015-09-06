79238e2e7f03a93571828a29319367000f9e29f4
Calvyn82 <claytonflesher@gmail.com>
2015-09-06 00:28:45 -0500

    deleting unused spec files

--- spec/controllers/tracker_controller_spec.rb
+++ spec/controllers/tracker_controller_spec.rb
@@ -1,4 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TrackerController, type: :controller do
+  it "creates a new bill subscription" do
+    skip "Haven't figured out how to write yet"
+  end
 end
