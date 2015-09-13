fe3175116e3e7c49bf531ac9d25785acafd132cf
Calvyn82 <claytonflesher@gmail.com>
2015-09-12 20:08:11 -0500

    made hash double quotes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,7 +62,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+   config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
@@ -79,11 +79,11 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings  = {
-    address:              'smtp.gmail.com',
+    address:              "smtp.gmail.com",
     port:                 587,
     domain:               "gmail.com",
     user_name:            ENV['EMAIL_USER_NAME'],
     password:             ENV['EMAIL_PASSWORD'],
-    authentication:       'plain',
+    authentication:       "plain",
     enable_starttls_auto: true }
 end
