13f53a9a48f4e4af3eed2fa43e721bf8a78f64ec
Calvyn82 <claytonflesher@gmail.com>
2015-10-04 14:02:03 -0500

    Repaced instances of User.new with FactoryGirl.build

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,39 +2,39 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it "makes sure the email is present" do 
-    user = User.new(email: nil)
+    user = FactoryGirl.build(:user, email: nil)
     expect(user).not_to                be_valid
     expect(user.errors[:email]).not_to be_empty
   end
 
   it "ensure the email is well-formed" do
-    user = User.new(email: "bad fake email")
+    user = FactoryGirl.build(:user, email: "bad fake email")
     expect(user).not_to                be_valid
     expect(user.errors[:email]).not_to be_empty
   end
 
   it "ensures the email is unique" do
     user1 = FactoryGirl.create(:user)
-    user2 = User.new(email: user1.email)
+    user2 = FactoryGirl.build(:user, email: user1.email)
     expect(user1).to                    be_valid
     expect(user2).not_to                be_valid
     expect(user2.errors[:email]).not_to be_empty
   end
 
   it "ensures the password is present when user created" do
-    user = User.new(password: nil)
+    user = FactoryGirl.build(:user, password: nil)
     expect(user).not_to                   be_valid
     expect(user.errors[:password]).not_to be_empty
   end
 
   it "ensures password confirmation is present on change" do
-    user = User.new(password: "password", password_confirmation: "")
+    user = FactoryGirl.build(:user, password: "password", password_confirmation: "")
     expect(user).not_to                                be_valid
     expect(user.errors[:password_confirmation]).not_to be_empty
   end
 
   it "ensures password is confirmed on change" do
-    user = User.new(password: "password", password_confirmation: "123")
+    user = FactoryGirl.build(:user, password: "password", password_confirmation: "123")
     expect(user).not_to                                be_valid
     expect(user.errors[:password_confirmation]).not_to be_empty
   end
