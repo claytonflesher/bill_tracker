dddef94fbc4b2a3d4da8e0a540e88bd1e23e0456
Calvyn82 <claytonflesher@gmail.com>
2015-10-04 20:16:04 -0500

    Improved test coverage in sessions controller spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,11 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  it "signs in a user" do
+  it "prevents invalid users from signing in" do
     user = FactoryGirl.create(:user)
     post :create, email: user.email, password: ""
     assert_response(:success)
     expect(session[:user_id]).not_to eq(user.id)
     expect(flash[:alert]).not_to be_nil 
+  end
+
+  it "signs in valid users" do
+    user = FactoryGirl.create(:user)
+    post :create, email: user.email, password: user.password
+    assert_response(:redirect)
+    expect(session[:user_id]).to eq(user.id)
+    expect(flash[:alert]).to be_nil 
   end
 end
