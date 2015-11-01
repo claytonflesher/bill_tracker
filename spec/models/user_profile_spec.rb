1f1662ec0b347d8f075bac56dbbc4f10bcbd5d45
Calvyn82 <claytonflesher@gmail.com>
2015-11-01 10:44:21 -0600

    changes to user_profile_spec

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -6,34 +6,4 @@ RSpec.describe UserProfile, type: :model do
     expect(profile).not_to                  be_valid
     expect(profile.errors[:address]).not_to be_empty
   end
-
-  it "has a senator" do
-    profile = FactoryGirl.build(:user_profile, senator: nil)
-    expect(profile).not_to                  be_valid
-    expect(profile.errors[:senator]).not_to be_empty
-  end
-
-  it "has a representative" do
-    profile = FactoryGirl.build(:user_profile, representative: nil)
-    expect(profile).not_to                         be_valid
-    expect(profile.errors[:representative]).not_to be_empty
-  end
-
-  it "has a senate district" do
-    profile = FactoryGirl.build(:user_profile, senate_district: nil)
-    expect(profile).not_to                          be_valid
-    expect(profile.errors[:senate_district]).not_to be_empty
-  end
-
-  it "has a house district" do
-    profile = FactoryGirl.build(:user_profile, house_district: nil)
-    expect(profile).not_to                         be_valid
-    expect(profile.errors[:house_district]).not_to be_empty
-  end
-
-  it "has a state" do
-    profile = FactoryGirl.build(:user_profile, state: nil)
-    expect(profile).not_to                 be_valid
-    expect(profile.errors[:state]).not_to be_empty
-  end
 end
