800d6be3c494554518c9f539de2dc4ccd2b33694
Calvyn82 <claytonflesher@gmail.com>
2016-01-03 11:58:44 -0600

    Stubbed geocoder out

--- spec/models/user_profile_spec.rb
+++ spec/models/user_profile_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe UserProfile, type: :model do
+  before(:all) do
+    Geocoder.configure(:lookup => :test)
+
+    Geocoder::Lookup::Test.add_stub(
+      "111 E Bay St., OKC, OK 73120", [
+        {
+          'latitude'  => 0,
+          'longitude' => 0
+        }
+      ]
+    )
+  end
   it "makes sure the street address is present" do
     profile = FactoryGirl.build(:user_profile, address: nil)
     expect(profile).not_to                  be_valid
